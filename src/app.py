245d52f4a67ac0a9e743619beba61a3672e9c9e4
Jeeva S. Chelladhurai <sjeeva@gmail.com>
2016-08-30 00:36:49 +0530

    added json interface

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 import socket
 import datetime
 
@@ -13,10 +14,10 @@ def show_details() :
     global startTime
     global hitCount
     hitCount = hitCount + 1
-    return "<html> " + \
+    return "<html>" + \
            "<head><title>Docker + Flask Demo</title></head>" + \
            "<body>" + \
-           "<table> "+ \
+           "<table>" + \
            "<tr><td> Start Time </td> <td>" +  startTime + "</td> </tr>" \
            "<tr><td> Hostname </td> <td>" + socket.gethostname() + "</td> </tr>" \
            "<tr><td> Local Address </td> <td>" + socket.gethostbyname(socket.gethostname()) + "</td> </tr>" \
@@ -25,6 +26,17 @@ def show_details() :
            "</table>" + \
            "</body>" + \
            "</html>"
+
+@app.route("/json")
+def send_json() :
+    global startTime
+    global hitCount
+    hitCount = hitCount + 1
+    return jsonify( {'StartTime' : startTime,
+                     'Hostname': socket.gethostname(),
+                     'LocalAddress': socket.gethostbyname(socket.gethostname()),
+                     'RemoteAddress':  request.remote_addr,
+                     'Server Hit': str(hitCount)} )
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0')
